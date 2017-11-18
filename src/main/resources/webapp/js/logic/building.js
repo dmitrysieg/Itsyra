4bf15cbf0e22a8bf71648b997da9fa7b26916659
d.boldyrev <dmitry.sieg@gmail.com>
2017-11-19 00:29:23 +0200

    Experiments with textures of level /b

--- src/main/resources/webapp/js/logic/building.js
+++ src/main/resources/webapp/js/logic/building.js
@@ -14,9 +14,24 @@ define([
         var roofHeight = 0.25 * sizeY;
 
         this.cementMaterial = new THREE.MeshPhongMaterial({color: 0x777777});
-        this.woodMaterial = new THREE.MeshPhongMaterial({color: 0x8B4513});
-        this.roofMaterial = new THREE.MeshPhongMaterial({color: 0xA52A2A, side: THREE.DoubleSide});
-        this.groundMaterial = new THREE.MeshPhongMaterial({color: 0x228B22, side: THREE.DoubleSide});
+
+        var wallTexture = new THREE.TextureLoader().load('images/brick.jpg');
+        wallTexture.wrapS = THREE.RepeatWrapping;
+        wallTexture.wrapT = THREE.RepeatWrapping;
+        wallTexture.repeat.set(4, 4);
+        this.wallMaterial = new THREE.MeshBasicMaterial({map: wallTexture});
+
+        var roofTexture = new THREE.TextureLoader().load('images/roof.jpg');
+        roofTexture.wrapS = THREE.RepeatWrapping;
+        roofTexture.wrapT = THREE.RepeatWrapping;
+        roofTexture.repeat.set(4, 4);
+        this.roofMaterial = new THREE.MeshBasicMaterial({map: roofTexture, side: THREE.DoubleSide});
+
+        var groundTexture = new THREE.TextureLoader().load('images/cobblestone.jpg');
+        groundTexture.wrapS = THREE.RepeatWrapping;
+        groundTexture.wrapT = THREE.RepeatWrapping;
+        groundTexture.repeat.set(4, 4);
+        this.groundMaterial = new THREE.MeshBasicMaterial({map: groundTexture, side: THREE.DoubleSide});
 
         this.group = new THREE.Group();
 
@@ -28,32 +43,32 @@ define([
         this.group.add(this.basement.mesh);
 
         // Mandatory element - floor
-        this.floor = {geometry: new THREE.CubeGeometry(sizeX, floorHeight, sizeZ), material: this.woodMaterial};
+        this.floor = {geometry: new THREE.CubeGeometry(sizeX, floorHeight, sizeZ), material: this.wallMaterial};
         this.floor.mesh = new THREE.Mesh(this.floor.geometry, this.floor.material);
 
         this.floor.mesh.position.setY(groundLevel + 0.5 * floorHeight);
         this.group.add(this.floor.mesh);
 
         // Mandatory elements - walls (supporting only rectangle now)
-        this.wall_1 = {geometry: new THREE.CubeGeometry(sizeX, sizeY, wallWidth), material: this.woodMaterial};
+        this.wall_1 = {geometry: new THREE.CubeGeometry(sizeX, sizeY, wallWidth), material: this.wallMaterial};
         this.wall_1.mesh = new THREE.Mesh(this.wall_1.geometry, this.wall_1.material);
         this.wall_1.mesh.position.setZ(-0.5 * sizeZ + 0.5 * wallWidth);
         this.wall_1.mesh.position.setY(0.5 * sizeY);
         this.group.add(this.wall_1.mesh);
 
-        this.wall_2 = {geometry: new THREE.CubeGeometry(sizeX, sizeY, wallWidth), material: this.woodMaterial};
+        this.wall_2 = {geometry: new THREE.CubeGeometry(sizeX, sizeY, wallWidth), material: this.wallMaterial};
         this.wall_2.mesh = new THREE.Mesh(this.wall_2.geometry, this.wall_2.material);
         this.wall_2.mesh.position.setZ(0.5 * sizeZ - 0.5 * wallWidth);
         this.wall_2.mesh.position.setY(0.5 * sizeY);
         this.group.add(this.wall_2.mesh);
 
-        this.wall_3 = {geometry: new THREE.CubeGeometry(wallWidth, sizeY, sizeZ), material: this.woodMaterial};
+        this.wall_3 = {geometry: new THREE.CubeGeometry(wallWidth, sizeY, sizeZ - 2.0 * wallWidth), material: this.wallMaterial};
         this.wall_3.mesh = new THREE.Mesh(this.wall_3.geometry, this.wall_3.material);
         this.wall_3.mesh.position.setX(-0.5 * sizeX + 0.5 * wallWidth);
         this.wall_3.mesh.position.setY(0.5 * sizeY);
         this.group.add(this.wall_3.mesh);
 
-        this.wall_4 = {geometry: new THREE.CubeGeometry(wallWidth, sizeY, sizeZ), material: this.woodMaterial};
+        this.wall_4 = {geometry: new THREE.CubeGeometry(wallWidth, sizeY, sizeZ - 2.0 * wallWidth), material: this.wallMaterial};
         this.wall_4.mesh = new THREE.Mesh(this.wall_4.geometry, this.wall_4.material);
         this.wall_4.mesh.position.setX(0.5 * sizeX - 0.5 * wallWidth);
         this.wall_4.mesh.position.setY(0.5 * sizeY);
@@ -77,13 +92,26 @@ define([
         this.roof.geometry.faces.push(new THREE.Face3(3, 4, 5));
         // bottom
         this.roof.geometry.faces.push(new THREE.Face3(0, 1, 4));
-        this.roof.geometry.faces.push(new THREE.Face3(1, 4, 3));
+        this.roof.geometry.faces.push(new THREE.Face3(0, 4, 3));
         // square sides
         this.roof.geometry.faces.push(new THREE.Face3(0, 2, 5));
         this.roof.geometry.faces.push(new THREE.Face3(0, 5, 3));
         this.roof.geometry.faces.push(new THREE.Face3(2, 1, 4));
         this.roof.geometry.faces.push(new THREE.Face3(2, 4, 5));
         this.roof.geometry.computeFaceNormals();
+
+        this.roof.geometry.faceVertexUvs[0] = [];
+        this.roof.geometry.faceVertexUvs[0].push([new THREE.Vector2(0, 0), new THREE.Vector2(1, 0), new THREE.Vector2(0.5, 0.5)]);
+        this.roof.geometry.faceVertexUvs[0].push([new THREE.Vector2(0, 0), new THREE.Vector2(1, 0), new THREE.Vector2(0.5, 0.5)]);
+
+        this.roof.geometry.faceVertexUvs[0].push([new THREE.Vector2(0, 0), new THREE.Vector2(1, 0), new THREE.Vector2(1, 1)]);
+        this.roof.geometry.faceVertexUvs[0].push([new THREE.Vector2(0, 0), new THREE.Vector2(1, 1), new THREE.Vector2(0, 1)]);
+
+        this.roof.geometry.faceVertexUvs[0].push([new THREE.Vector2(1, 0), new THREE.Vector2(1, 1), new THREE.Vector2(0, 1)]);
+        this.roof.geometry.faceVertexUvs[0].push([new THREE.Vector2(1, 0), new THREE.Vector2(0, 1), new THREE.Vector2(0, 0)]);
+        this.roof.geometry.faceVertexUvs[0].push([new THREE.Vector2(0, 1), new THREE.Vector2(0, 0), new THREE.Vector2(1, 0)]);
+        this.roof.geometry.faceVertexUvs[0].push([new THREE.Vector2(0, 1), new THREE.Vector2(1, 0), new THREE.Vector2(1, 1)]);
+        this.roof.geometry.uvsNeedUpdate = true;
 
         this.roof.mesh = new THREE.Mesh(this.roof.geometry, this.roof.material);
         this.group.add(this.roof.mesh);
@@ -99,6 +127,19 @@ define([
         this.ground.geometry.faces.push(new THREE.Face3(0, 1, 3));
         this.ground.geometry.faces.push(new THREE.Face3(0, 3, 2));
         this.ground.geometry.computeFaceNormals();
+
+        this.ground.geometry.faceVertexUvs[0] = [];
+        this.ground.geometry.faceVertexUvs[0].push([
+            new THREE.Vector2(0, 0),
+            new THREE.Vector2(0, 1),
+            new THREE.Vector2(1, 1)
+        ]);
+        this.ground.geometry.faceVertexUvs[0].push([
+            new THREE.Vector2(0, 0),
+            new THREE.Vector2(1, 1),
+            new THREE.Vector2(1, 0)
+        ]);
+        this.ground.geometry.uvsNeedUpdate = true;
 
         this.ground.mesh = new THREE.Mesh(this.ground.geometry, this.ground.material);
         this.group.add(this.ground.mesh);
